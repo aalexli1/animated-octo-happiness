56750a7fc953e9aaa964a4f0d1af62e3a4c75c2a
Alex Li <alexander.y.li2000@gmail.com>
2025-08-16 23:51:28 -0700

    Add initial iOS project

---
+++ animated-octo-happiness-ios/animated-octo-happiness-ios/animated_octo_happiness_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  animated_octo_happiness_iosApp.swift
+//  animated-octo-happiness-ios
+//
+//  Created by Alex on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct animated_octo_happiness_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
